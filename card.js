e8a3d15152fdfdf53a03b62cf5fd1297b1eed234
LaurentSchall <laurentschallfonteilles@gmail.comm>
2023-02-15 18:14:49 +0100

    init project

---
+++ card.js
@@ -0,0 +1,12 @@
+const cardGuerrierProtection = {
+    role : "tank",
+    specialisation : "protection",
+    classe : 'guerrier',
+    }
+
+function displayCardTank (role){
+    console.log("iama" + role)
+  }
+
+
+  displayCardTank(role)
