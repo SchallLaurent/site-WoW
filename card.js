ea8d5bcb795498beb99802cf5392dad56ef2e198
LaurentSchall <laurentschallfonteilles@gmail.comm>
2023-02-18 16:37:42 +0100

    ok

--- card.js
+++ card.js
@@ -1,12 +1,30 @@
-const cardGuerrierProtection = {
-    role : "tank",
-    specialisation : "protection",
-    classe : 'guerrier',
-    }
+// const tabWar = [{
+//     nom : "guerrier",
+//     talent : "protection",
+// },{
+//     nom : "guerrier",
+//     talent : "arme",
+// },{
+//     nom : "guerrier",
+//     talent : "fury", 
+// }]
+// // pour chaque valeurs du tableau War, afficher les valeurs dans containerWar et incrémenter les valeurs nom et talent
+// for(let value of tabWar){
+//     document.querySelector('.containerWar').innerHTML += `${value.nom} ${value.talent}`;
 
-function displayCardTank (role){
-    console.log("iama" + role)
-  }
+
+//  }
+    
+document.querySelector(".containerWar").style.backgroundColor = "#371506";
 
 
-  displayCardTank(role)
+
+
+
+   
+
+
+
+
+
+
